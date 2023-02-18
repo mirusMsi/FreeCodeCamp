4100d6c8c0ac97949a2f2dd330f22705467b2bd5
webmirus <miroshkin.mirus@ya.ru>
2023-02-18 17:32:18 +0300

    1. The MAIN_MENU() function was created 2. In MAIN_MENU(), the ability to output a list of services from the services table has been added

--- Relational Database/Build a Salon Appointment Scheduler/salon.sh
+++ Relational Database/Build a Salon Appointment Scheduler/salon.sh
@@ -3,3 +3,17 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ Salon Appointment Scheduler ~~~~~\n"
+
+
+MAIN_MENU() {
+
+  SERVICES_LIST=$($PSQL "SELECT * FROM services;")
+  echo "$SERVICES_LIST" | while read SERVICE_ID BAR SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+  
+}
+
+
+MAIN_MENU
