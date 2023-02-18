1c51bc87cc4a83ec17be5f32be6a3611d172dacc
webmirus <miroshkin.mirus@ya.ru>
2023-02-18 18:11:43 +0300

    In MAIN_MENU(): 1. Added the ability to return to the beginning of the script execution with the output of a message. 2. Added a check for the validity of entering the service number by the type of input and the presence of the service number in the database.

--- Relational Database/Build a Salon Appointment Scheduler/salon.sh
+++ Relational Database/Build a Salon Appointment Scheduler/salon.sh
@@ -5,25 +5,39 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ Salon Appointment Scheduler ~~~~~\n"
 
 
+TRY_="Please try again."
+
+
 MAIN_MENU() {
 
-  SERVICES_LIST=$($PSQL "SELECT * FROM services;")
+  if [[ $1 ]]
+  then
+    echo -e "\n$1\nPlease try again.\n\n"
+  fi
+
+  SERVICES_LIST=$($PSQL "SELECT * FROM services ORDER BY service_id;")
   echo "$SERVICES_LIST" | while read SERVICE_ID BAR SERVICE_NAME
   do
     echo "$SERVICE_ID) $SERVICE_NAME"
   done
 
-  echo -e "\n0) Exit"
-
   echo -e "\nPlease enter the number of the service you want to sign up for:"
-  read SERVICE_ID_SELECTED
+  read SERVICE_ID_SELECTED; EXIT $SERVICE_ID_SELECTED
 
-  EXIT $SERVICE_ID_SELECTED
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  then
+    MAIN_MENU "That is not a valid service number."
+  else
+    if [[ -z $($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED") ]]
+    then
+      MAIN_MENU "Sorry, we don't have any service available right now."
+    fi
+  fi
   
 }
 
 EXIT() {
-  if [[ $1 == 0 ]]
+  if [[ $1 == q ]]
   then
     echo -e "\nThank you for visit."
     exit
