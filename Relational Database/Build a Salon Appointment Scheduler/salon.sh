bfe5b64d69af5e73e147ccbab00cdd31b8c5cef1
webmirus <miroshkin.mirus@ya.ru>
2023-02-18 19:01:27 +0300

    In MAIN_MENU(): 1. Added a request for the client's phone number 2. Added the ability to add a new client to the database in his absence 3. Added the ability to make an appointment

--- Relational Database/Build a Salon Appointment Scheduler/salon.sh
+++ Relational Database/Build a Salon Appointment Scheduler/salon.sh
@@ -15,22 +15,65 @@ MAIN_MENU() {
     echo -e "\n$1\nPlease try again.\n\n"
   fi
 
-  SERVICES_LIST=$($PSQL "SELECT * FROM services ORDER BY service_id;")
+  SERVICES_LIST=$($PSQL "
+    SELECT * 
+    FROM services
+    ORDER BY service_id;
+  ")
   echo "$SERVICES_LIST" | while read SERVICE_ID BAR SERVICE_NAME
   do
     echo "$SERVICE_ID) $SERVICE_NAME"
   done
 
   echo -e "\nPlease enter the number of the service you want to sign up for:"
-  read SERVICE_ID_SELECTED; EXIT $SERVICE_ID_SELECTED
+  read SERVICE_ID_SELECTED
+  
+  #EXIT $SERVICE_ID_SELECTED
 
   if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
   then
     MAIN_MENU "That is not a valid service number."
   else
-    if [[ -z $($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED") ]]
+    SERVICE_NAME=$($PSQL "
+      SELECT name
+      FROM services
+      WHERE service_id = $SERVICE_ID_SELECTED;
+    ")
+    if [[ -z $SERVICE_NAME ]]
     then
       MAIN_MENU "Sorry, we don't have any service available right now."
+    else
+      echo -e "\nWhat's your phone number?"
+      read CUSTOMER_PHONE
+
+      CUSTOMER_ID=$($PSQL "
+        SELECT customer_id
+        FROM customers
+        WHERE phone = '$CUSTOMER_PHONE';
+      ")
+      if [[ -z $CUSTOMER_ID ]]
+      then
+        echo -e "\nWhat's your name?"
+        read CUSTOMER_NAME
+
+        INSERT_CUSTOMER_NAME=$($PSQL "
+          INSERT INTO customers(phone, name)
+          VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME');
+        ")
+        CUSTOMER_ID=$($PSQL "
+          SELECT customer_id
+          FROM customers
+          WHERE phone = '$CUSTOMER_PHONE';
+        ")
+      fi
+
+      echo -e "\nPlease enter the desired service delivery time"
+      read SERVICE_TIME
+
+      INSERT_APPOINTMENT=$($PSQL "
+        INSERT INTO appointments(customer_id, service_id, time)
+        VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME');
+      ")
     fi
   fi
   
